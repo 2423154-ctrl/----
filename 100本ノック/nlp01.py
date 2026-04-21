261ff69391d027092aa08b70c677258fb251db5c
2423154-ctrl <2423154@mwu.jp>
2026-04-21 16:27:55 +0900

    変更

--- 100本ノック/nlp01.py
+++ 100本ノック/nlp01.py
@@ -1,5 +1,25 @@
+#00
 a="パトカー"
 b="タクシー"
+ab=a[0]+b[0]+a[1]+b[1]+a[2]+b[2]+a[3]+b[3]
+print(ab)
+#01
+abs=ab[1]+ab[3]+ab[5]+ab[7]
+print(abs)
+#02
+s="stressed"
+#03
+l="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+list1=l.split()
+print(list1)
+list2=[]
+list3=[]
+for i in range(len(list1)):
+    list2=list1[i]
+    list3.append(list2[0])
+print(list3)
+#04
+l="Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+listA=l.split()
 
-print(a[0]+b[0]+a[1]+b[1]+a[2]+b[2]+a[3]+b[3])
-print(a)
+
